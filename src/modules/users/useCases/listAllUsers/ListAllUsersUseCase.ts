9753d39082b3c9dffa5b881c8a2242b7531dbd00
leandrocl2005 <leandrocl2005@yahoo.com.br>
2021-03-21 17:00:33 -0300

    first try

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,15 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+    if (!user) {
+      throw new Error("User doesn't exists");
+    }
+    if (!user.admin) {
+      throw new Error("User doesn't have permission");
+    }
+    const users = this.usersRepository.list();
+    return users;
   }
 }
 
